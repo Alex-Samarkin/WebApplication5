0d8f5f4e11ec8e97fc2dea2aff8f867cd242d68c
alex_ <alexsamarkinru@gmail.com>
2021-03-27 12:39:02 +0300

    Добавьте файлы проекта.

---
+++ WebApplication5/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5
+{
+    public class Person
+    {
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public string Middlename { get; set; } = "";
+        public string Surname { get; set; } = "";
+        public DateTime BirthDate { get; set; } = DateTime.Today;
+        public string Passport { get; set; } = "0000-000000";
+        public string Phone { get; set; } = "8-(000)-123-12-12";
+
+
+    }
+}
